74d05c8bbd45a42feb93a12fda0612365a7023cb
bm2530696 <bmonroe2@student.rcc.edu>
2015-01-06 21:20:42 -0800

    Signed-off-by: bm2530696 <bmonroe2@student.rcc.edu>

---
+++ Hmwk/ComputerMakeMistakes/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Breanna Monroe
+ * Created on January 6, 2015, 9:04 PM
+ * Purpose: Our first program
+ */
+
+//System Libraries 
+#include <iostream> 
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here!
+int main (int argc, char** argv) {
+    //Declare variables
+    float a,b,c;
+   //Prompt user for a and b
+    cout<<"Input 2 float variable values"<<endl;cin>>a>>b;
+    //Calculate the sum
+    c=a+b;
+    //Output the results
+    cout<<"The sum c=a+b ->"<<c<<endl;
+    //Exit Stage right!
+}
+
+
